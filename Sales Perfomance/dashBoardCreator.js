d2ddcee5ebaa019c7d6026ced0a339aa2acb991d
Kamalraj M M <kamaljp@gmail.com>
2022-10-14 18:49:41 +0530

    Added the basic data to the dashboard

--- Sales Perfomance/dashBoardCreator.js
+++ Sales Perfomance/dashBoardCreator.js
@@ -1,97 +1,64 @@
 const dataAquisition = async ()=>{
-    const salesTarget = await d3.csv('salesTarget.csv')
-    const productsTarget = await d3.csv('productsTarget.csv')
-    const monthlyTargets = await d3.csv('monthlyTargets.csv')
-    const customerAcquisition = await d3.csv('customerAcquisition.csv')
-    const adsProcurement = await d3.csv('adsProcurement.csv')
-    // console.log(adsProcurement)
+    const salesPerformance = await d3.csv('salesPerformanceDashboard.csv')
+    console.log(salesPerformance)
     // console.log(customerAcquisition)
     
     //Parsing and cleaning Data
-    const parseDate = d3.timeParse("%B %d, %Y");
+    const parseDate = d3.timeParse("%d-%m-%Y");
     const numberFormat = d3.format(".2s")
 
-    const cleanSalesData = salesTarget.map(d =>({
-        date : parseDate(d.Date),
-        product: d["Prod..s"].replace(' ','').replace('.','_'),
-        revenue:+d.Revenue.replace('$','').replace(',','')
-    }))
-    const cleanAdsData = adsProcurement.map(d =>({
-        date : parseDate(d.Date),
-        adsExpense:+d['Ads Expenses'].replace('$','').replace(',','')
-    }))
-    const cleanCustomerAcqData = customerAcquisition.map(d =>({
-        date : parseDate(d.f),
-        cstAcqrd: +d["New Customer"]
-        
-    }))
-
-    //getting total 
-    const totalSales = d3.sum(cleanSalesData, d => d.revenue)
-    const totalCstAqrd = d3.sum(cleanCustomerAcqData, d => d.cstAcqrd)
-    //checking the totals then sending data to DOM
-    // console.log(totalCstAqrd,totalSales,totaladsExp)
-    dataSentId(numberFormat(totalSales),'revenueAch')
-    dataSentId(numberFormat(totalCstAqrd),'customerAch')
-
-    const monthlySalesTgt = monthlyTargets.filter(d => d.Activity == 'Sales Target').map(d => ({
-        months: d.months,
-        Target: +d.Target,
-        Revenue: +d.Revenue
-    }))
-    const monthlyCustTgt = monthlyTargets.filter(d => d.Activity == 'Customer Acquisition').map(d => ({
-        months: d.months,
-        Target: +d.Target,
-        Revenue: +d.Revenue
-    }))
-    const monthlyAdsTgt = monthlyTargets.filter(d => d.Activity == 'Ads Budget and expenses').map(d => ({
-        months: d.months,
-        Budget: +d.Target,
-        Expense: +d.Revenue
-    }))
-    console.log(monthlyAdsTgt)
-    const cleanProductsTarget = productsTarget.map(d => ({
-        Products: d.Products.replace(' ','').replace('.','_'),
-        Target: +d.Target,
-        Revenue: +d.Revenue
+    const cleanSalePerfData = salesPerformance.map(d=>({
+        cogs: Number(d.COGS),
+        category: d.Category,
+        cost: Number(d.Cost),
+        customerName: d['Customer Name'],
+        genter: d.Gender,
+        grossProfit: d['Gross Profit'],
+        price: Number(d.Price),
+        product: d.Product,
+        qty: Number(d.Qty),
+        dateSold: parseDate(d['Sales Date']),
+        saleId:d['Sales ID'],
+        salesRep: d['Sales Reps'],
+        stores:d.Stores,
+        totalSales: d['Total Sales'],
+        txnType: d['Trans.Types'],
+        weekDays:d['Week days']
     }))
 
-    const totalSalesTgt = d3.sum(monthlySalesTgt, d => d.Target)
+    // console.log(cleanSalePerfData)
+    // Aggregate data points
+    var totQtySold, totSales, totCogs, totProfit, totCustomers, totTransaction, totProducts, totStores;
 
-    const totaladsExpTgt = d3.sum(monthlyAdsTgt, d => d.Budget)
-    const totaladsExp = d3.sum(monthlyAdsTgt, d => d.Expense)
+    totQtySold = sumSeries(cleanSalePerfData,'qty')
+    totSales= sumSeries(cleanSalePerfData,'totalSales')
+    totCogs= sumSeries(cleanSalePerfData,'cogs')
+    totProfit= sumSeries(cleanSalePerfData,'grossProfit')
+    totTransaction= cleanSalePerfData.length,
 
-    const totalCstAqrdTgt = d3.sum(monthlyCustTgt, d => d.Target)
+    //Grouping based on customers
+    totCustomers= d3.rollups(cleanSalePerfData,v => v.length, d => d.customerName).length;
+    totProducts= d3.rollups(cleanSalePerfData,v => v.length, d => d.product).length;
+    totStores= d3.rollups(cleanSalePerfData,v => v.length,d => d.stores).length;
+    totReps = d3.rollups(cleanSalePerfData,v => v.length,d => d.salesRep).length;
+
+    // console.log(totQtySold, totSales, totCogs, totProfit, totCustomers, totTransaction, totProducts, totStores)
+
+    dataSentId(numberFormat(totQtySold),"qty")
+    dataSentId(numberFormat(totSales),"sales")
+    dataSentId(numberFormat(totCogs),"cost")
+    dataSentId(numberFormat(totProfit),"profit")
+    dataSentId(numberFormat(totCustomers),"cust")
+    dataSentId(numberFormat(totProducts),"pdt")
+    dataSentId(numberFormat(totStores),"str")
+    dataSentId(numberFormat(totTransaction),"txn")
     
-    const totalProductTgt = d3.sum(cleanProductsTarget, d => d.Target)
-    const totalProductRev = d3.sum(cleanProductsTarget, d => d.Revenue)
-    // console.log(numberFormat(totalSalesTgt))
-    dataSentId(numberFormat(totalProductTgt),'pdtTgt')
-    dataSentId(numberFormat(totalProductRev),'pdtAch')
-    dataSentId(numberFormat(totaladsExp),'adsAch')
-
-    dataSentId(numberFormat(totalSalesTgt),'revenueTgt')
-    dataSentId(numberFormat(totalCstAqrdTgt),'customerTgt')
-    dataSentId(numberFormat(totaladsExpTgt),'adsTgt')
-
-    const salesAchievement = (totalSales / totalSalesTgt) * 100;
-    const acquisitionAchievement = (totalCstAqrd / totalCstAqrdTgt) * 100;
-    const adsExpenseAchievement = (totaladsExp / totaladsExpTgt) * 100
-    const productWiseAchievement = (totalProductRev / totalProductTgt) * 100;
-    dataSentId(`Achieved ${salesAchievement.toFixed(1)}%`,'achvRevn')
-    dataSentId(`Achieved ${acquisitionAchievement.toFixed(1)}%`,'achvCust')
-    dataSentId(`Achieved ${productWiseAchievement.toFixed(1)}%`,'achvPdt')
-    dataSentId(`Consumed ${adsExpenseAchievement.toFixed(1)}%`,'achvAds')
-
-    buildGroupedBar(monthlySalesTgt, 'asset1Chart')
-    buildStackedBar(monthlyCustTgt,'asset2Chart')
-    scatterPlot(cleanProductsTarget,'asset3Chart','Products','Revenue','Target','purple','orange')
-    // buildCompositeChart(cleanProductsTarget,'asset3Chart')
-    // buildCompositeChart(adsExpenseAchievement,'asset4Chart')
-    lineBarPlot(monthlyAdsTgt,'asset4Chart','months','Expense','Budget','purple','orange')
-    // console.log(cleanProductsTarget)
-
 }   
+
+function sumSeries(dataset, series){
+    let sum = d3.sum(dataset, d => d[series])
+    return sum
+}
 
 function dataSentId(value, id){
     let elt = document.getElementById(id)
